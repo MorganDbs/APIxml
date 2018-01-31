04cf579aa22b06f7e5a879e71722e90dfb96e999
Steven W <email@email.fr>
2018-01-31 17:44:57 +0100

    .

--- src/public/assets/app/app.js
+++ src/public/assets/app/app.js
@@ -1,79 +1,62 @@
 window.Interoperabilite = (() => {
-    let module = {};
-    let map, parkingIcon;
+	let module = {};
+	let map, parkingIcon;
 
-    module.init = () => {
-        let context = this;
-        this.parkingIcon = L.icon({
-            iconUrl: 'public/assets/leaflet/images/parking.png',
+	module.init = (position, markers) => {
+		$.ajaxPrefilter((options) => {
+			if (options.crossDomain) {
+				console.log(options.url)
+				options.url = "tcp://www-cache:3128/" + encodeURIComponent(options.url);
+				console.log(options.url)
+				options.crossDomain = false;
+			}
+		});
 
-            iconSize:     [32, 37],
-            iconAnchor:   [16, 35],
-            popupAnchor:  [0, -35]
-        });
+		let context = this;
+		this.parkingIcon = L.icon({
+			iconUrl: 'public/assets/leaflet/images/parking.png',
 
-        let pos = module.query('http://ip-api.com/json', 'GET', 'json')
-        pos.done((position) => {
-            this.map = L.map('map').setView([position.lat, position.lon], 13);
-            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(this.map);
+			iconSize:     [32, 37],
+			iconAnchor:   [16, 35],
+			popupAnchor:  [0, -35]
+		});
 
-            L.marker([position.lat, position.lon]).addTo(this.map)
-                .bindPopup('Votre position')
-                .openPopup();
+		this.map = L.map('map').setView([position.lat, position.lon], 13);
+		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+			attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(this.map);
 
-            let prParkings = module.query('http://www.velostanlib.fr/service/carto', 'GET', 'xml')
-            prParkings.done((data) => {
-                $(data).find('marker').each((k, v) => {
-                    module.addParking(v);
-                });                
-            });
-        });       
-    }
+		L.marker([position.lat, position.lon]).addTo(this.map)
+		.bindPopup('Votre position')
+		.openPopup();
 
-    module.query = (url, method, dataType, data = null) => {
-        let pr = $.ajax(url, {
-            type: method,
-            dataType: dataType,
-            context: this,
-            data: data
-        });
-        pr.fail((jqXHR, status, error) => {
-           console.log(error)
-        })
-        return pr;
-    }
+		module.addParking(markers);
+	}
 
-    module.addParking = (data) => {
-        let number = $(data).attr('number')
-        let nom = $(data).attr('name')
-        let adresse = $(data).attr('address')
-        let ouvert = ($(data).attr('open') == 1 ? 'Ouvert' : 'Fermé');
-        
-        let station = module.query(`http://www.velostanlib.fr/service/stationdetails/nancy/${number}`, 'GET', 'xml')
-        station.done((s) => {
-            let disponible = $(s).find('available').text();
-            let libres = $(s).find('free').text();
-            let total = $(s).find('total').text();
+	module.addParking = (data) => {
+		$.each(data, (k, p) => {
+			let number = p.number;
+			let nom = p.name;
+			let adresse = p.address;
+			let ouvert = (p.open == 1 ? 'Ouvert' : 'Fermé');
+			
+			let disponible = p.station.available;
+			let libres = p.station.free;
+			let total = p.station.total;
 
-            L.marker([$(data).attr('lat'), $(data).attr('lng')], {
-                icon: this.parkingIcon
-            })
-            .addTo(this.map)
-            .bindPopup(`
-                <h6>${nom}</h6>
-                <p>Adresse : ${adresse}</p>
-                <p>Vélos : ${disponible} / ${total}</p>
-                <p>Places libres : ${libres}</p>
-                <p>${ouvert}</p>
-            `); 
-        });
-    }
+			L.marker([p.lat, p.lng], {
+				icon: this.parkingIcon
+			})
+			.addTo(this.map)
+			.bindPopup(`
+				<h6>${nom}</h6>
+				<p>Adresse : ${adresse}</p>
+				<p>Vélos : ${disponible} / ${total}</p>
+				<p>Places libres : ${libres}</p>
+				<p>${ouvert}</p>
+			`); 
+		})
+	}
 
-    return module;
+	return module;
 })();
-
-$(() => {
-	Interoperabilite.init();
-});
